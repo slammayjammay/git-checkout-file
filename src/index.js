6001c1c30a256b31be26b8feaa2e3a4b76e24a29
Scott McKenzie <slammayjammay@gmail.com>
2017-02-01 17:24:18 -0800

    Use git-files for matching

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
 const execSync = require('child_process').execSync
+const gitFiles = require('git-files')
 
 let globs = process.argv.slice(2)
+let files = gitFiles.all()
+let matchedFiles = {}
 
 for (let glob of globs) {
-	execSync(`git checkout -- *${glob}*`)
+	// case-insensitive for now
+	let regex = new RegExp(glob, 'i')
+	let matches = files.filter(file => regex.test(file))
+	matches.forEach(file => matchedFiles[file] = true)
+
+}
+
+for (let file of matchedFiles) {
+	execSync(`git checkout -- ${file}`)
 }
